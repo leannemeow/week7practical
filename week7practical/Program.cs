790b9752bde1cd3304bed3f1927c5fb8b1aa5c04
Leanne Alhussein <Alhussein-L@ulster.ac.uk>
2025-11-04 10:43:08 +0300

    initial2

--- week7practical/Program.cs
+++ week7practical/Program.cs
@@ -10,6 +10,62 @@ namespace week7practical
     {
         static void Main(string[] args)
         {
+            char[] alphabet = new char[26];
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(97 + i);
+                Console.WriteLine(alphabet[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = alphabet.Length; --i >= 0;)
+            {
+                Console.WriteLine(alphabet[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = Char.ToUpper(alphabet[i]);
+                Console.WriteLine(alphabet[i]);
+            }
+            Console.WriteLine();
+
+
+            int[] fibonacci = { 12, 1, 2, 3, 5, 8, 1, 34, 21, 55 };
+            int max = 0;
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                if (fibonacci[i] > max)
+                { 
+                    max = fibonacci[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine();
+
+            Array.Sort(fibonacci);
+            foreach (int i in fibonacci)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            int toFind = 34;
+            int index = 0;
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                if (fibonacci[i] == toFind)
+                {
+                    index = i; 
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+
 
         }
     }
