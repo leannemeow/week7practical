b4630647fb24449dab36a235eb3622c9801861f8
Leanne Alhussein <Alhussein-L@ulster.ac.uk>
2025-11-04 11:42:16 +0300

    khals

---
+++ My2DArrayProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My2DArrayProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] alphabet = new char[26, 2];
+
+            int row = alphabet.GetLength(0);
+            int col = alphabet.GetLength(1);
+            Console.WriteLine(row);
+            Console.WriteLine(col);
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (j == 0)
+                    {
+                        alphabet[i, j] = (char)(65 + i);
+                    } 
+                    else
+                    {
+                        alphabet[i, j] = (char)(97 + i);
+                    }
+                }
+            }
+
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Console.Write(alphabet[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- MyListProject/Program.cs
+++ MyListProject/Program.cs
@@ -10,7 +10,43 @@ namespace MyListProject
     {
         static void Main(string[] args)
         {
+            List<string> months = new List<string>();
 
+            months.Add("Jan");
+            months.Add("Feb");
+            months.Add("Mar");
+            months.Add("Apr");
+            months.Add("May");
+            months.Add("Jun");
+            months.Add("Jul");
+            months.Add("Aug");
+            months.Add("Sep");
+            months.Add("Oct");
+            months.Add("Nov");
+            months.Add("Dec");
+
+            string[] monthsName = months.ToArray();
+
+            for (int i = 0; i < monthsName.Length; i++)
+            {
+                Console.WriteLine(monthsName[i]);
+            }
+            Console.WriteLine();
+
+            int befLeng = months.Count;
+            Console.WriteLine(befLeng);
+            Console.WriteLine();
+
+            months.Remove("Sep");
+            months.Remove("Nov");
+
+            int aftLeng = months.Count;
+            Console.WriteLine(aftLeng);
+            Console.WriteLine();
+
+            string findMonth = "Dec";
+            int index = months.IndexOf(findMonth);
+            Console.WriteLine(index);
         }
     }
 }
